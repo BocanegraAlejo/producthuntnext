85748b83efd84dd8f3e84e5e62fd9d3cfbb9171f
4l3003 <44855372+4l3003@users.noreply.github.com>
2021-01-07 17:39:00 -0300

    deploy

---
+++ firebase/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAs4WPrMOFKWls-PbqkBDsh-x13J4ykVek",
+    authDomain: "producthunt-44ce4.firebaseapp.com",
+    projectId: "producthunt-44ce4",
+    storageBucket: "producthunt-44ce4.appspot.com",
+    messagingSenderId: "698117109146",
+    appId: "1:698117109146:web:7c2f5a766b9f61520cf623"
+  };
+
+export default firebaseConfig;
